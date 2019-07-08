50e7dce28ef32a0435fd3780760a366a72b971e6
PAL-2 <venkatakrishna_s@dell.com>
2019-07-09 02:34:49 +0530

    PAL tracker .net core web api

--- src/PalTracker/WelcomeController.cs
+++ src/PalTracker/WelcomeController.cs
@@ -6,6 +6,6 @@ namespace PalTracker
     public class WelcomeController : ControllerBase
     {
         [HttpGet]
-        public string SayHello() => "hello Laxman/Krishna";
+        public string SayHello() => "hello";
     }
 }
